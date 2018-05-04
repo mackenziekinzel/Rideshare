b29715688ff9f40e962ae203781315e44c60b04f
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-05-03 23:24:50 -0400

    hello github

---
+++ Rideshare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rideshare
+//
+//  Created by Mackenzie Kinzel on 5/1/18.
+//  Copyright © 2018 Mackenzie Kinzel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
