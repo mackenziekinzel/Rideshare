e29844fc0c88fa0d72dde09441587754723aea9c
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-05-04 23:13:26 -0400

    create GradientView

---
+++ Rideshare/View/GradientView.swift
@@ -0,0 +1,27 @@
+//
+//  GradientView.swift
+//  Rideshare
+//
+//  Created by Mackenzie Kinzel on 5/4/18.
+//  Copyright © 2018 Mackenzie Kinzel. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    let gradient = CAGradientLayer()
+    
+    override func awakeFromNib() {
+        setUpGradientView()
+    }
+    
+    func setUpGradientView() {
+        gradient.frame = self.bounds
+        gradient.colors = [UIColor.white.cgColor, UIColor.init(white: 1.0, alpha: 0.0).cgColor]
+        gradient.startPoint = CGPoint.zero
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        gradient.locations = [0.8, 1.0]
+        self.layer.addSublayer(gradient)
+    }
+}
