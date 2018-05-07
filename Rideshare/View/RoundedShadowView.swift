74f6e843f06feb367816da3ca3812d11d2804d08
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-05-06 23:02:10 -0400

    create RoundedShadowView

---
+++ Rideshare/View/RoundedShadowView.swift
@@ -0,0 +1,24 @@
+//
+//  RoundedShadowView.swift
+//  Rideshare
+//
+//  Created by Mackenzie Kinzel on 5/6/18.
+//  Copyright © 2018 Mackenzie Kinzel. All rights reserved.
+//
+
+import UIKit
+
+class RoundedShadowView: UIView {
+    
+    override func awakeFromNib() {
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = 5.0
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOffset = CGSize(width: 0, height: 5)
+    }
+}
