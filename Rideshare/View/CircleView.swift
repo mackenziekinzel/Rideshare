6c3a5fee11f5de1708f4b30d4bf1efcbfbb57b71
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-05-06 23:10:53 -0400

    create CircleView

---
+++ Rideshare/View/CircleView.swift
@@ -0,0 +1,28 @@
+//
+//  CircleView.swift
+//  Rideshare
+//
+//  Created by Mackenzie Kinzel on 5/6/18.
+//  Copyright © 2018 Mackenzie Kinzel. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIView {
+
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            setupView()
+        }
+    }
+    
+    override func awakeFromNib() {
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.layer.borderWidth = 1.5
+        self.layer.borderColor = borderColor?.cgColor
+    }
+}
