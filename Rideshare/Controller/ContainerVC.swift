b45b489066810efa4a7774dddac67f2d368a6268
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-05-10 23:33:25 -0400

    create ContainerVC

---
+++ Rideshare/Controller/ContainerVC.swift
@@ -0,0 +1,33 @@
+//
+//  ContainerVC.swift
+//  Rideshare
+//
+//  Created by Mackenzie Kinzel on 5/10/18.
+//  Copyright © 2018 Mackenzie Kinzel. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class ContainerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
+private extension UIStoryboard {
+    class func mainStoryboard() -> UIStoryboard {
+        return UIStoryboard(name: "Main", bundle: Bundle.main)
+    }
+    
+    class func leftViewController() -> LeftSidePanelVC? {
+        return mainStoryboard().instantiateViewController(withIdentifier: "LeftSidePanelVC") as? LeftSidePanelVC
+    }
+    
+    class func homeVC() -> HomeVC? {
+        return mainStoryboard().instantiateViewController(withIdentifier: "HomeVC") as? HomeVC
+    }
+}
