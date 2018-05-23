38ccd057d3d2086a7a623865c7811aa01707114e
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-05-22 23:40:44 -0400

    setup segue to LoginVC

--- Rideshare/Controller/LeftSidePanelVC.swift
+++ Rideshare/Controller/LeftSidePanelVC.swift
@@ -21,15 +21,10 @@ class LeftSidePanelVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginButtonWasPressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginVC") as? LoginVC
+        present(loginVC!, animated: true, completion: nil)
     }
-    */
-
+    
 }
