38ffb04579f27da0c0210bfebad2a6bdefc70822
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-06-06 23:10:52 -0400

    create handleScreenTap func to dismiss keyboard when view is tapped

--- Rideshare/Controller/LoginVC.swift
+++ Rideshare/Controller/LoginVC.swift
@@ -13,6 +13,13 @@ class LoginVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.bindToKeyboard()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleScreenTap(sender:)))
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleScreenTap(sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
     }
 
     @IBAction func cancelButtonWasPressed(_ sender: Any) {
