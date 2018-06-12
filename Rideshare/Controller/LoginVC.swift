c0fd387826895d7bcffac16747ae8b621ba24b2f
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-06-11 23:44:47 -0400

    complete authButtonWasPressed func

--- Rideshare/Controller/LoginVC.swift
+++ Rideshare/Controller/LoginVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController, UITextFieldDelegate {
     
@@ -39,6 +40,24 @@ class LoginVC: UIViewController, UITextFieldDelegate {
         if emailField.text != nil && passwordField.text != nil {
             authButton.animateButton(shouldLoad: true, withMessage: nil)
             self.view.endEditing(true)
+            
+            if let email = emailField.text, let password = passwordField.text {
+                Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                    if error == nil {
+                        if let user = user {
+                            if self.segmentedControl.selectedSegmentIndex == 0 {
+                                let userData = ["provider": user.providerID] as [String: Any]
+                                DataService.instance.createFirebaseDBUser(uid: user.uid, userData: userData, isDriver: false)
+                            } else {
+                                let userData = ["provider": user.providerID, "userIsDriver": true, "isPickupModeEnabled": false, "driverIsOnTrip": false] as [String: Any]
+                                DataService.instance.createFirebaseDBUser(uid: user.uid, userData: userData, isDriver: true)
+                            }
+                        }
+                        print("Email user authenticated successfully with Firebase")
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+            }
         }
     }
 }
