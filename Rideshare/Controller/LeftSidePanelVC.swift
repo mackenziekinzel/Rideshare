f530c03bb71e82527f504e123eda9aea7a2beeff
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-06-15 23:39:22 -0400

    create observePassengersAndDrivers func in LeftSidePanelVC

--- Rideshare/Controller/LeftSidePanelVC.swift
+++ Rideshare/Controller/LeftSidePanelVC.swift
@@ -7,11 +7,55 @@
 //
 
 import UIKit
+import Firebase
 
 class LeftSidePanelVC: UIViewController {
 
+    @IBOutlet weak var pickupModeSwitch: UISwitch!
+    @IBOutlet weak var pickupModeLabel: UILabel!
+    @IBOutlet weak var userImageView: RoundImageView!
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var accountTypeLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        pickupModeSwitch.isOn = false
+        pickupModeSwitch.isHidden = true
+        pickupModeLabel.isHidden = true
+        
+        observePassengersAndDrivers()
+    }
+    
+    func observePassengersAndDrivers() {
+        DataService.instance.REF_USERS.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshot {
+                    if snap.key == Auth.auth().currentUser?.uid {
+                        self.accountTypeLabel.text = "PASSENGER"
+                    }
+                }
+            }
+        })
+        
+        DataService.instance.REF_DRIVERS.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshot {
+                    if snap.key == Auth.auth().currentUser?.uid {
+                        self.accountTypeLabel.text = "DRIVER"
+                        self.pickupModeSwitch.isHidden = false
+                        let switchStatus = snap.childSnapshot(forPath: "isPickupModeEnabled").value as! Bool
+                        self.pickupModeSwitch.isOn = switchStatus
+                        self.pickupModeLabel.isHidden = false
+                    }
+                }
+            }
+        })
     }
     
     @IBAction func loginButtonWasPressed(_ sender: Any) {
