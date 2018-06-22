3f7a3f50945f4653b5e6b4b8e53a2ccb88e9997e
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-06-21 23:26:07 -0400

    create UpdateService to update user locations

---
+++ Rideshare/Services/UpdateService.swift
@@ -0,0 +1,41 @@
+//
+//  UpdateService.swift
+//  Rideshare
+//
+//  Created by Mackenzie Kinzel on 6/21/18.
+//  Copyright © 2018 Mackenzie Kinzel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+import Firebase
+
+class UpdateService {
+    static var instance = UpdateService()
+    
+    func updateUserLocation(withCoordinate coordinate: CLLocationCoordinate2D) {
+        DataService.instance.REF_USERS.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let userSnapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for user in userSnapshot {
+                    if user.key == Auth.auth().currentUser?.uid {
+                        DataService.instance.REF_USERS.child(user.key).updateChildValues(["coordinate": [coordinate.latitude, coordinate.longitude]])
+                    }
+                }
+            }
+        })
+    }
+    
+    func updateDriverLocation(withCoordinate coordinate: CLLocationCoordinate2D) {
+        DataService.instance.REF_DRIVERS.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let driverSnapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for driver in driverSnapshot {
+                    if driver.key == Auth.auth().currentUser?.uid {
+                        if driver.childSnapshot(forPath: "isPickupModeEnabled").value as? Bool == true { DataService.instance.REF_DRIVERS.child(driver.key).updateChildValues(["coordinate": [coordinate.latitude, coordinate.longitude]])
+                        }
+                    }
+                }
+            }
+        })
+    }
+}
