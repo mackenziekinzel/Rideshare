45bbf46f3d43a7e939ab2cd6a3068f4ec50d8130
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-07-14 23:41:58 -0400

    create PickupVC class

---
+++ Rideshare/Controller/PickupVC.swift
@@ -0,0 +1,19 @@
+//
+//  PickupVC.swift
+//  Rideshare
+//
+//  Created by Mackenzie Kinzel on 7/14/18.
+//  Copyright © 2018 Mackenzie Kinzel. All rights reserved.
+//
+
+import UIKit
+
+class PickupVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
