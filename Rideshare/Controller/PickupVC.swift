ce899a1c60f15d32b3f85fe3e2870238f9bab606
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-07-24 23:25:38 -0400

    create MKMapViewDelegate extension in PickupVC

--- Rideshare/Controller/PickupVC.swift
+++ Rideshare/Controller/PickupVC.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import MapKit
 
 class PickupVC: UIViewController {
     
     @IBOutlet weak var pickupMapView: RoundMapView!
+    
+    var regionRadius: CLLocationDistance = 2000
+    var pin: MKPlacemark? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,5 +26,36 @@ class PickupVC: UIViewController {
     
     @IBAction func cancelButtonPressed(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension PickupVC: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "pickupPoint"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        } else {
+            annotationView?.annotation = annotation
+        }
+        annotationView?.image = UIImage(named: "destinationAnnotation")
+        return annotationView
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+        pickupMapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func dropPinFor(placemark: MKPlacemark) {
+        pin = placemark
+        
+        for annotation in pickupMapView.annotations {
+            pickupMapView.removeAnnotation(annotation)
+        }
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        pickupMapView.addAnnotation(annotation)
     }
 }
